d5d9825ede505377ac1ce9cf5026f0ce8ad8085a
Ванин Илия <i.vanin@adata.kz>
2022-06-30 15:56:42 +0600

    Added a solve for task 3 sub 2

--- Task3/index2.php
+++ Task3/index2.php
@@ -1,13 +1,14 @@
 <?php
-$string1 = 'c:\WebServers\home\testsite\www\myfile.txt';
+$string1 = 'c:\WebServers\home\testsite\www\my.file..txt';
 $string2 = '/var/log/myfile2.txt';
 $string3 = 'c:\WebServers\home\testsite\www\myfile.7z';
 $string4 = 'c:\WebServers\home\testsite\www\myfile';
 
 function infopath($path){
+    $pattern = "/\.[a-zA-Z]+$/ui";
     $path = str_replace("\\" ,"/", $path);
     $pathfile = basename($path);
+    $pathfile = preg_replace($pattern," ",$pathfile);
     return $pathfile;
-
 }
-echo infopath($string4);
+echo infopath($string2);
