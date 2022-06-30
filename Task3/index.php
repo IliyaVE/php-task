f48c8e0e320e96e0763c39b0ad3bc8f01bd27d12
IVanin <21824@adata.kz>
2022-06-30 14:27:25 +0600

    Initial commit

--- Task3/index.php
+++ Task3/index.php
@@ -1,7 +1,10 @@
 <?php
-$string = 'c:\WebServers\home\testsite\www\myfile.txt';
+$string1 = 'c:\WebServers\home\testsite\www\myfile.txt';
+$string2 = '/var/log/myfile.txt';
+$string3 = 'c:\WebServers\home\testsite\www\myfile.7z';
+$string4 = 'c:\WebServers\home\testsite\www\myfile';
 
-$regexp = "/[a-zA-Z0-9]+.txt$/ui";
+$regexp = "/[a-zA-Z0-9]+.[a-zA-Z]$/ui";
 
 preg_match($regexp, $string, $match);
 
