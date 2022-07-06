90ce98b324f02f67fe46af13308b7f803fa0f2b1
Iliya <vanin.iliya@gmail.com>
2022-07-06 15:48:04 +0600

    Added a solve for task 4

--- Task4/action.php
+++ Task4/action.php
@@ -1,36 +1,22 @@
 <?php
-$link = mysqli_connect("localhost:8001", "root", "test", "test_php");
+$link = mysqli_connect('localhost:3306' , 'iliya','test','test_php');
 
-if ($link == false) {
-    print("Ошибка: Невозможно подключиться к Базе Данных" . mysqli_connect_error());
-    exit;
+if ($link == false){
+    print("Ошибка: Невозможно подключиться в Базе Данных". mysqli_connect_error());
 }
-$LastName = $_POST["LastName"];
-$firstName = $_POST["FirstName"];
-$Years = $_POST["Years"] ;
-$City = $_POST["City"] ;
-$NumberClass = $_POST["NumberClass"] ;
-$Grands = $_POST["Grands"] ;
+
+
+// $ID = $_POST[''];
+$LastName = $_POST['LastName'];
+$FirstName = $_POST['FirstName'];
+$Years = $_POST['Years'];
+$City = $_POST['City'];
+$NumberClass = $_POST['NumberClass'];
+$Grands = $_POST['Grands'];
 $CountGrands = 10000;
-$Grades = $_POST["Grades"] ;
-$ScoreOfUniversity = $_POST["ScoreOfUniversity"] ;
-$sql =  "INSERT INTO list_students VALUES(NULL,'$LastName','$firstName',$Years,'$City',$NumberClass,$Grands,$CountGrands,$Grades,$ScoreOfUniversity)";
-
-#$num = '20';
-#$varch = 'sad';
-#$sql =  "INSERT INTO test_name_1 VALUES($num,'$varch')";
-$result = mysqli_query($link, $sql);
+$Grades = $_POST['Grades'];
+$ScoreOfUniversity = $_POST['ScoreOfUniversity'];
+$sql = "INSERT INTO test_php.test_php VALUES ('NULL','$LastName','$FirstName','$Years','$City','$NumberClass','$Grands','$CountGrands','$Grades','$ScoreOfUniversity')";
 
 
-if (!$result) {
-    echo "Произошла ошибка.\n";
-    exit;
-}
-
-
-
-
-
-
-
-
+$result = mysqli_query($link,$sql);
